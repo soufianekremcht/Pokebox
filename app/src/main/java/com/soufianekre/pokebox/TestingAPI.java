964f9c8f9a011a4d7a1030633ffb87b4423e03be
SoufianeKre <prosoufiane2000@gmail.com>
2021-07-04 17:27:50 +0100

    Fix Gradle bugs

--- app/src/main/java/com/soufianekre/pokebox/TestingAPI.java
+++ app/src/main/java/com/soufianekre/pokebox/TestingAPI.java
@@ -2,67 +2,66 @@ package com.soufianekre.pokebox;
 
 public class TestingAPI {
 
+  /*
 
-    /*
+  public static void  main(String[] args) {
+      int POKEMON_ID = 1;
+      PokeApiClient pokeApi = new PokeApiClient();
 
-    public static void  main(String[] args) {
-        int POKEMON_ID = 1;
-        PokeApiClient pokeApi = new PokeApiClient();
+      // Pokemon Info
 
-        // Pokemon Info
+      Pokemon simplePokemon = pokeApi.getPokemon(POKEMON_ID);
+      List<PokemonStat> pokemonStats = simplePokemon.getStats();
+      List<PokemonType> types  = simplePokemon.getTypes();
 
-        Pokemon simplePokemon = pokeApi.getPokemon(POKEMON_ID);
-        List<PokemonStat> pokemonStats = simplePokemon.getStats();
-        List<PokemonType> types  = simplePokemon.getTypes();
+      System.out.println("Pokemone Name : " + simplePokemon.getName() + "-> Base Exp : "+simplePokemon.getBaseExperience());
 
-        System.out.println("Pokemone Name : " + simplePokemon.getName() + "-> Base Exp : "+simplePokemon.getBaseExperience());
+      // Pokemon Icon
+      System.out.println("Pokemon Icons :" +simplePokemon.getSprites().getFrontShiny());
 
-        // Pokemon Icon
-        System.out.println("Pokemon Icons :" +simplePokemon.getSprites().getFrontShiny());
+      System.out.println("Pokemon Type : ");
+      for (PokemonType type : types){
+          System.out.println(type.getType().getName());
+      }
+      System.out.println("Pokemon Stats :");
+      for ( PokemonStat stat : pokemonStats){
+          System.out.println("Stat " + stat.getStat() + " -> Effort " + stat.getBaseStat());
+      }
+      //Contests Info
+      ContestType contestName = pokeApi.getContestType(1);
 
-        System.out.println("Pokemon Type : ");
-        for (PokemonType type : types){
-            System.out.println(type.getType().getName());
-        }
-        System.out.println("Pokemon Stats :");
-        for ( PokemonStat stat : pokemonStats){
-            System.out.println("Stat " + stat.getStat() + " -> Effort " + stat.getBaseStat());
-        }
-        //Contests Info
-        ContestType contestName = pokeApi.getContestType(1);
-
-        System.out.println(contestName.getNames());
-        System.out.println(contestName.getBerryFlavor().getName());
-
-
-
-        // Locations Info
-        int i = 1;
-        while (i <14){
-            Location pokeLocation = pokeApi.getLocation(i);
-            print("Location " + i);
-            System.out.println(pokeLocation.getNames());
-            i++;
-
-        }
-
-        System.out.println("Pokemon Evolution : ");
-        EvolutionChain evolutionChain = pokeApi.getEvolutionChain(POKEMON_ID);
-        print("Evolution Chain : " + evolutionChain.getChain().getEvolutionDetails());
+      System.out.println(contestName.getNames());
+      System.out.println(contestName.getBerryFlavor().getName());
 
 
 
+      // Locations Info
+      int i = 1;
+      while (i <14){
+          Location pokeLocation = pokeApi.getLocation(i);
+          print("Location " + i);
+          System.out.println(pokeLocation.getNames());
+          i++;
 
-        // Evolutions Info
+      }
 
-        //System.out.println(bulbasaur.getStats().toString());
-    }
-
-
-    public static void print(Object o){
-        System.out.println(o);
-    }
+      System.out.println("Pokemon Evolution : ");
+      EvolutionChain evolutionChain = pokeApi.getEvolutionChain(POKEMON_ID);
+      print("Evolution Chain : " + evolutionChain.getChain().getEvolutionDetails());
 
 
-     */
+
+
+      // Evolutions Info
+
+      //System.out.println(bulbasaur.getStats().toString());
+  }
+
+
+  public static void print(Object o){
+      System.out.println(o);
+  }
+
+
+   */
 }
